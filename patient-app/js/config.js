fa89a94054255a23b602cea60d39536482ad418a
Peniel-source <p.obeng@alustudent.com>
2026-07-30 00:07:35 +0200

    changed api base urls

--- patient-app/js/config.js
+++ patient-app/js/config.js
@@ -1,2 +1,2 @@
 // I'll change this once the backend is live at api.penworks.tech
-const API_BASE_URL = 'http://localhost:3000/api/v1';
+const API_BASE_URL = "https://api.penworks.tech/api/v1";
--- responder-app/js/config.js
+++ responder-app/js/config.js
@@ -1,1 +1,1 @@
-const API_BASE_URL = 'http://localhost:3000/api/v1';
+const API_BASE_URL = "https://api.penworks.tech/api/v1";
